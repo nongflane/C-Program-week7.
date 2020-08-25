fb19b0b134ebaddcae99460448c42f0e4494c607
nongflane <69183471+nongflane@users.noreply.github.com>
2020-08-25 09:34:25 +0700

    Add files via upload

---
+++ myC101.c
@@ -0,0 +1,25 @@
+/*Program Name : program to Start
+Author : Mr.Chadayut tharanurak
+Date : 8/18263  
+*/
+#include<stdio.h>//link from library stdio.h
+void message();//funtion prototype declaration section
+void cat();//function prototype declaration section
+
+void main(){
+    int anlmal=5;//Variable declaration
+    printf("Hello World\n");//Executetable part
+    printf("%i",animal);//Executetable part
+    message();Function call
+    cat();Function call
+}
+
+//Sub function : message
+void message(){
+    printf("We can take sub function\n");
+}
+
+//Sub function : cat 
+void cat(){
+    printf("I LOVE My cat\n");  
+}
